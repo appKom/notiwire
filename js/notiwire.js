e3781525e626300f97de069a2184f3e30efd8a0c
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-10-27 20:55:54 +0100

    Prompt for lightstatus is now supported

--- js/notiwire.js
+++ js/notiwire.js
@@ -3,6 +3,8 @@ var cantina = require("./cantina.js");
 var office = require("./office.js");
 var http = require("http");
 var hackerspace = require("./hackerspace.js");
+var officeLight = require("./officeLight.js");
+var prompt = require("prompt");
 
 var server = http.createServer(function(req, res){
 	var urlInfo = req.url.split("/");
@@ -10,27 +12,43 @@ var server = http.createServer(function(req, res){
 		if (urlInfo.length == 2){
 			if (urlInfo[1] == "info" || urlInfo[1] == "help" || urlInfo[1] == ""){
 				res.end(getHelpText());
+				console.log("inforequest");
 			}
 			else if (urlInfo[1] == "hackerspace"){
 				hackerspace.get(function(isOpen){
 					res.end(isOpen);
+					console.log("hackerspacerequest");
 				});
+			}
+			else if (urlInfo[1] == "favicon.ico"){
+				res.end("No favicon.ico");
+				console.log("Favicon requested");
+			}
+
+			else{
+				res.end("ERROR: Length 2");
+				console.log("ERROR: Length 2");
 			} 
 		}
-		else
+		else{
 			res.end("Something wrong happend!");
+			console.log("ERROR");
+		}
 	}
 	else if (urlInfo[1] === "coffee"){
 		coffee.get(true, urlInfo[2], function(tx1, tx2){
 			res.end(tx1 + "\n" + tx2);
+			console.log("Coffee for " + urlInfo[2]);
 		});
 	}else if (urlInfo[1] === "office"){
-		office.get(urlInfo[2], function(status, message){
+		office.get(urlInfo[2], function(status, message, light){
 			res.end(status + "\n" + message);
+			console.log("Office for " + urlInfo[2] + "\nLight: " + light);
 		});
 	}
 	else {
 		res.end("Wrong input");
+		console.log("ERROR: Wrong input");
 	}
 });
 
@@ -42,6 +60,15 @@ function getHelpText(){
 }
 
 server.listen(80);
+
+prompt.start();
+
+prompt.get(["Info", "Affilation"], function(err, res){
+	officeLight.get(res.Affilation, function(data){
+		console.log(res.Affilation + " light: " + data);
+	});
+});
+
 
 // coffee.get(true, function(text1, text2){
 // 	console.log(text1);
