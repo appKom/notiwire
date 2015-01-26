31890d5b1d23445db68739830323e97e8286e761
halv00rsen <jorgen-halvorsen@hotmail.com>
2015-01-26 21:30:18 +0100

    Light, meeting and coffee is now supported

--- notiwire/routes/index.js
+++ notiwire/routes/index.js
@@ -3,12 +3,38 @@ var router = express.Router();
 var office = require("../../js/office.js");
 var cantina = require("../../js/cantina.js");
 var hackerspace = require("../../js/hackerspace.js");
-
+var coffee = require("../../js/coffee.js");
+var meetings = require("../../js/meetings.js");
+var officeLight = require("../../js/officeLight.js");
 /* GET home page. */
 router.get('/office/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.route("/meeting/:affiliation").get(function(req, res){
+    meetings.get(req.params.affiliation, function(meeting){
+        res.json({
+          "status": meeting
+        });
+    });
+});
+
+router.route("/light/:affiliation").get(function(req, res){
+    officeLight.get(req.params.affiliation, function(data){
+        res.json({
+            "status": data
+        });
+    });
+});
+
+router.route("/coffee/:affiliation").get(function(req, res){
+    coffee.get(false, req.params.affiliation, function(pots, age){
+        res.json({
+          "status":pots,
+          "time":age
+        });
+    });
+});
 router.route('/office/:affiliation').get(function(req, res) {
     office.get(req.params.affiliation, function(status, message) {
         res.json({
