c18c20d1d290c4694b454bc7ff19640bfb5a864e
Christian Duvholt <cxhristian@gmail.com>
2015-03-04 16:16:25 +0100

    Reindent

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ var coffee = require("../libs/coffee.js");
 var meetings = require("../libs/meetings.js");
 
 var httpErrorStatus = function(data, res) {
-    if(data.error) {
-        res.status(400);
-    }
+  if(data.error) {
+    res.status(400);
+  }
 };
 
 /* GET home page. */
@@ -20,45 +20,45 @@ router.get('/', function(req, res) {
 });
 
 router.route('/office/:affiliation').get(function(req, res) {
-    async.parallel([
-        function(callback) {
-          coffee.get(req.params.affiliation, function(data){
-              callback(null, {name: 'coffee', value: data});
-          });
-        },
-        function(callback) {
-          office.getEventData(req.params.affiliation, function(data) {
-              callback(null, {name: 'event', value: data});
-          });
-        },
-        function(callback) {
-          office.getLightData(req.params.affiliation, function(data) {
-              callback(null, {name: 'status', value: data});
-          });
-        }
-      ],
-      function(err, results) {
-        var data = {};
-        results.forEach(function(result) {
-          data[result.name] = result.value;
-        });
-        res.json(data);
-      }
+  async.parallel([
+    function(callback) {
+      coffee.get(req.params.affiliation, function(data){
+        callback(null, {name: 'coffee', value: data});
+      });
+    },
+    function(callback) {
+      office.getEventData(req.params.affiliation, function(data) {
+        callback(null, {name: 'event', value: data});
+      });
+    },
+    function(callback) {
+      office.getLightData(req.params.affiliation, function(data) {
+        callback(null, {name: 'status', value: data});
+      });
+    }
+    ],
+    function(err, results) {
+      var data = {};
+      results.forEach(function(result) {
+        data[result.name] = result.value;
+      });
+      res.json(data);
+    }
     );  
 });
 
 router.route('/hackerspace').get(function(req, res) {
-    hackerspace.get(function(data) {
-        httpErrorStatus(data, res);
-        res.json(data);
-    });
+  hackerspace.get(function(data) {
+    httpErrorStatus(data, res);
+    res.json(data);
+  });
 });
 
 router.route('/cantina/:location').get(function(req, res) {
-    cantina.get(req.params.location, function(data) {
-        httpErrorStatus(data, res);
-        res.json(data);
-    });
+  cantina.get(req.params.location, function(data) {
+    httpErrorStatus(data, res);
+    res.json(data);
+  });
 });
 
 module.exports = router;
