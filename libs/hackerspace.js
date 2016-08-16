af1b7259c0fff3c3e15c662bda1be691f00c8925
Christian Duvholt <cxhristian@gmail.com>
2016-08-17 00:31:52 +0200

    Update hackerspace api with new endpoint

--- libs/hackerspace.js
+++ libs/hackerspace.js
@@ -5,21 +5,21 @@ var Hackerspace = function() {
   this.debug = 0;
 
   this.web = 'https://hackerspace-ntnu.no/';
-  this.api = 'https://hackerspace-ntnu.no/api/door';
-  
+  this.api = 'https://hackerspace-ntnu.no/door/get_status/';
+
   this.msgDisconnected = 'Frakoblet fra Hackerspace';
   this.msgError = 'Malformatert data fra Hackerspace';
   this.responseData = {};
 };
-  
+
 Hackerspace.prototype.get = function(callback) {
   var self = this;
   requests.json(self.api, {
     success: function(door) {
       if (self.debug) console.log('Raw door:\n\n', door);
 
-      if (typeof door === 'object') {
-        self.responseData.open = door.isOpen.door;
+      if (typeof door === 'string') {
+        self.responseData.open = door === 'True';
       }
       else {
         // Empty string returned from API
@@ -29,7 +29,7 @@ Hackerspace.prototype.get = function(callback) {
     },
     error: function(jqXHR, text, err) {
       if (self.debug) console.log('ERROR: Failed to get hackerspace info.');
-      data.error = self.msgDisconnected;
+      self.responseData.error = self.msgDisconnected;
       callback(self.responseData);
     }
   });
