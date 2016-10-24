b2ac43b91eafca5683a179be60d67638c28ff02c
Christian Duvholt <cxhristian@gmail.com>
2016-10-25 01:09:17 +0200

    Const and fat arrow in app.js

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 #!/usr/bin/env node
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var debug = require('debug')('notiwire');
-var express = require('express');
-var favicon = require('serve-favicon');
-var http = require('http');
-var log4js = require("log4js");
-var path = require('path');
-var nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const debug = require('debug')('notiwire');
+const express = require('express');
+const favicon = require('serve-favicon');
+const http = require('http');
+const log4js = require("log4js");
+const path = require('path');
+const nunjucks = require('nunjucks');
 
 
-var api = require('./routes/api');
-var docs = require('./routes/docs');
-var notipi = require('./routes/notipi');
+const api = require('./routes/api');
+const docs = require('./routes/docs');
+const notipi = require('./routes/notipi');
 
 // Logging
 log4js.configure(__dirname + "/logging.json");
-var logger = log4js.getLogger();
+const logger = log4js.getLogger();
 
-var app = express();
+const app = express();
 // MongoDB
-var db = require('monk')('localhost/notiwire');
+const db = require('monk')('localhost/notiwire');
 // WebSockets
-var io = require('socket.io')(server);
-var server = http.Server(app);
+const server = http.Server(app);
+const io = require('socket.io')(server);
 
-io.on('connection', function(socket) {
+io.on('connection', socket => {
     console.log("Client connected.");
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', () => {
         console.log("Lost client.");
     });
 });
@@ -51,7 +51,7 @@ app.use(cookieParser());
 
 
 // Access to db and websockets in routes
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
    req.db = db;
    req.io = io;
    next();
@@ -63,8 +63,8 @@ app.use('/api', api);
 app.use('/notipi', notipi);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
@@ -74,7 +74,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use((err, req, res, next) => {
         logger.info(err);
         res.status(err.status || 500);
         res.json({
@@ -87,7 +87,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
     if(err.status !== 404) {
         logger.error(err);
     }
@@ -100,6 +100,6 @@ app.use(function(err, req, res, next) {
 
 app.set('port', process.env.PORT || 3000);
 
-server.listen(app.get('port'), function() {
+server.listen(app.get('port'), () => {
   debug('Express server listening on port ' + server.address().port);
 });
